c5145f18fdb056a964a5e5111caca7e1379cd494
Roman <rrkarimov@gmail.com>
2024-11-07 23:11:43 +0500

    dbo.users

--- node-pool/src/sql/database.sql
+++ node-pool/src/sql/database.sql
@@ -5,7 +5,15 @@
 CREATE TABLE IF NOT EXISTS public.users
 (
     id integer NOT NULL DEFAULT nextval('users_id_seq'::regclass),
-    name text COLLATE pg_catalog."default",
-    email text COLLATE pg_catalog."default",
+    username text COLLATE pg_catalog."default",
+    firstname text COLLATE pg_catalog."default",
+    lastname text COLLATE pg_catalog."default",
+    password text COLLATE pg_catalog."default",
+    role text COLLATE pg_catalog."default",
     CONSTRAINT users_pkey PRIMARY KEY (id)
 )
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.users
+    OWNER to postgres;
