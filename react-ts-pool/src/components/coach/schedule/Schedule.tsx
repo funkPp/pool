59f9fc6422ecc558c852102c40eb2b2ebd0b6155
Roman <rrkarimov@gmail.com>
2025-01-21 23:09:07 +0500

    fix-me culture-RU

--- react-ts-pool/src/components/coach/schedule/Schedule.tsx
+++ react-ts-pool/src/components/coach/schedule/Schedule.tsx
@@ -1,18 +1,35 @@
-import { Calendar, momentLocalizer } from "react-big-calendar";
+import { Calendar, momentLocalizer, DateLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+require("globalize/lib/cultures/globalize.culture.ru-RU");
 
-const localizer = momentLocalizer(moment);
+const mlocalizer = momentLocalizer(moment);
 const myEventsList = [
   {
-    id: 0,
+    id: 10,
     title: "Тестовое событие",
     start: new Date(2025, 0, 21, 9, 0, 0),
-    end: new Date(2025, 0, 21, 13, 0, 0),
-    resourceId: [1, 2],
+    end: new Date(2025, 0, 21, 10, 30, 0),
+    resourceId: [1],
   },
 ];
-export function Schedule() {
+
+const cultures = "ru-RU";
+const messages = {
+  week: "Неделя",
+  work_week: "Рабочая неделя",
+  day: "День",
+  month: "Месяц",
+  previous: "Назад",
+  next: "Вперед",
+  today: "Сегодня",
+  agenda: "План",
+  date: "Дата",
+  time: "Время",
+  event: "Группа",
+};
+
+export function Schedule({ localizer }: { localizer: DateLocalizer }) {
   return (
     <div>
       <h1 className="p-1 font-semibold text-cyan-600 text-center">
@@ -21,11 +38,13 @@ export function Schedule() {
       <div className="flex flex-row columns-2">
         <div className="w-3/4 ">
           <Calendar
+            culture={cultures}
+            messages={messages}
             localizer={localizer}
             events={myEventsList}
             startAccessor="start"
             endAccessor="end"
-            style={{ height: 600 }}
+            style={{ height: 500 }}
           />
         </div>
         <div className="w-1/4 border"></div>
--- react-ts-pool/src/components/coach/schedule/ScheduleLayout.tsx
+++ react-ts-pool/src/components/coach/schedule/ScheduleLayout.tsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import { Schedule } from "./Schedule";
 
+import { momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+const mlocalizer = momentLocalizer(moment);
+
 export function ScheduleLayout() {
   return (
     <div className="px-4">
       <div className="container">
         <Routes>
-          <Route index element={<Schedule />} />
+          <Route index element={<Schedule localizer={mlocalizer} />} />
           {/* <Route path="add" element={<AddEdit />} />
           <Route path="edit/:id" element={<AddEdit />} /> */}
         </Routes>
